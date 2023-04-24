b2360a83a41d54ff3938889755887ae773a15687
Érica Garcez <112111829+Japolina@users.noreply.github.com>
2023-04-24 19:30:35 -0300

    Add files via upload

---
+++ Proj1/conta-bancaria.js
@@ -0,0 +1,54 @@
+const usuario = {
+    nome: "CSL",
+    transacoes: [],
+    saldo: 0
+}
+function criarTransacao(transacao){
+
+    usuario.transacoes.push(transacao)
+
+    // Verificar o tipo de transacao
+    if (transacao.tipo == "deposito") {
+        usuario.saldo = usuario.saldo + transacao.valor
+    }else if (transacao.tipo == "saque"){
+        usuario.saldo = usuario.saldo - transacao.valor
+    }
+
+}
+
+criarTransacao({tipo: 'deposito', valor: 9000})
+criarTransacao({tipo: 'saque', valor: 500})
+
+console.log({...usuario, saldo: usuario.saldo.toFixed(2) } );
+
+//extra
+
+function mostrarMaiorTransacaoPorTipo(tipo){
+    let maiorValor = 0
+    //verificar transações de maior valor
+    for (let transacao of usuario.transacoes){
+    if (transacao.valor > maiorValor && transacao.tipo == tipo ){
+        maiorValor  = transacao.valor
+        console.log(transacao.valor);
+    }
+  }
+  return {tipo: tipo, valor: maiorValor}
+}
+console.log(mostrarMaiorTransacaoPorTipo('saque')); 
+
+function monstrarValorMedioDasTransacoes (){
+    //verificar media das transações
+    var soma = 0
+
+    for (let transacao of usuario.transacoes){
+        soma = soma + transacao.valor
+
+        
+    }
+
+    var media = soma / usuario.transacoes.length
+
+    return media.toFixed(2)
+
+  }
+  console.log(monstrarValorMedioDasTransacoes());
